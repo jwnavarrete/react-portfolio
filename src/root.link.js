c284ca0c576497328ee33e20cf42b66a4cf98bb8
John Navarrete <jhon.navarrete@generali.com.ec>
2022-09-10 13:09:04 -0500

    cambio en configuracion}

--- src/root.link.js
+++ src/root.link.js
@@ -1,11 +1,11 @@
 export const RESUME_PATH = 'https://drive.google.com/';
 export const GITHUB_USERNAME = 'codewithvk';
 export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
-export const FIRST_NAME = 'Vivekkumar';
-export const LAST_NAME = 'Javiya';
-export const ADDRESS = 'Rajkot, Gujrat'
-export const EMAIL_ID = 'work.vivekjaviya@gmail.com';
+export const FIRST_NAME = 'John';
+export const LAST_NAME = 'Navarrete';
+export const ADDRESS = 'Ecuador, Duran'
+export const EMAIL_ID = 'jwnavarretez@gmail.com';
 export const TWITTER_ID = 'Vk_javiya';
-export const FRONT_PAGE_NAME = 'Vivek Kumar';
+export const FRONT_PAGE_NAME = 'John Navarrete';
 export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
 
